159f49267954933e59e41b04d40ae84cf788fb75
techhtml <apes0123@gmail.com>
2021-12-21 21:45:57 +0900

    ♻️ Wine API 연동

--- src/pages/wines/port.tsx
+++ src/pages/wines/port.tsx
@@ -1,28 +1,13 @@
 import type { NextPage } from "next";
-import { Error, Loading, WineCard } from "../../components";
-import { useWineData } from "../../hooks/useWineData";
-import { Wine } from "../../types/Wine";
+import { WineContainer } from "../../components/";
 
 const PortWinePage: NextPage = () => {
   const name = 'port';
-  const { data, error } = useWineData(name)
-
-  if(error) return <Error />
-  if(!data) return <Loading />
 
   return (
     <div>
-      <h1>Wine</h1>
-      <main>
-        {data.map((wineData: Wine) => {
-          return (
-            <WineCard 
-              key={`port-wine-list-${wineData.id}`} 
-              wineData={wineData}
-            />
-          )
-        })}
-      </main>
+      <h1>{name} wine</h1>
+      <WineContainer name={name} />
     </div>
   )
 }
--- src/pages/wines/whites.tsx
+++ src/pages/wines/whites.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from "next";
+import { WineContainer } from "../../components/";
 
 const WhitesWinePage: NextPage = () => {
+  const name = 'whites';
+
   return (
     <div>
-      <h1>Wine</h1>
+      <h1>{name} wine</h1>
+      <WineContainer name={name} />
     </div>
   )
 }
