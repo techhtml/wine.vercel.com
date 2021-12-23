6a9fe663395f9d13c9e4041be1e81859d805f1b5
techhtml <apes0123@gmail.com>
2021-12-23 19:51:21 +0900

    💄 WineContainer 스타일 작업

--- src/components/WineContainer.tsx
+++ src/components/WineContainer.tsx
@@ -1,4 +1,6 @@
+import styled from "@emotion/styled";
 import { Error, Loading, WineCard } from ".";
+import { MEDIA_QUERY_END_POINT } from "../constants";
 import { useWineData } from "../hooks/useWineData";
 import { Wine } from "../types/Wine";
 
@@ -14,8 +16,8 @@ export const WineContainer = ({ name }: WineContainerProps) => {
   
   return (
     <div>
-      <h1>{name} wine</h1>
-      <main>
+      <WineTitle>{name} wine</WineTitle>
+      <WineCardContainer>
         {data.map((wineData: Wine) => {
           return (
             <WineCard 
@@ -24,7 +26,24 @@ export const WineContainer = ({ name }: WineContainerProps) => {
             />
           )
         })}
-      </main>
+      </WineCardContainer>
     </div>
   )
 }
+
+const WineTitle = styled.h1`
+  line-height: 180%;
+  border-bottom: 1px solid #eee;
+`
+
+const WineCardContainer = styled.main`
+  display: grid;
+
+  @media (min-width: ${MEDIA_QUERY_END_POINT.MOBILE}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: ${MEDIA_QUERY_END_POINT.TABLET}) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,9 @@
 export const WINE_API_ENDPOINT = 'https://api.sampleapis.com/wines/';
 export const BEER_API_ENDPOINT = 'https://api.sampleapis.com/beers/';
+
+export const MEDIA_QUERY_END_POINT = {
+  MOBILE: '640px',
+  TABLET: '1280px',
+  DESKTOP: '1920px',
+  LARGE: '2560px'
+}
